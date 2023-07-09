28106de61cd47a305cc194425b8d0a456b38947f
dokyu <dokyung36@naver.com>
2023-07-09 10:46:59 +0900

    Commit message 각주 추가

--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -16,7 +16,8 @@ public class HelloResponseDtoTest {
         HelloResponseDto dto = new HelloResponseDto(name, amount);
 
         //then
-        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getName()).isEqualTo(name); //검증하고 싶은 대상을 메소드 인자로 받음
+        //테스트 검증 라이브러리의 검증 메소드
         assertThat(dto.getAmount()).isEqualTo(amount);
     }
 }
