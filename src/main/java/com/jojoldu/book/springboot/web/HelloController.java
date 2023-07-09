8f29fa1e26d0aed449b4dc22185ef52586265d85
dokyu <dokyung36@naver.com>
2023-07-09 10:46:30 +0900

    Commit message 각주 추가

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //컨트롤러를 json을 반환하는 컨트롤러로 변환
 public class HelloController {
 
     @GetMapping("/hello")
@@ -20,3 +20,5 @@ public class HelloController {
     }
 
 }
+
+//@RequestParam : 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
