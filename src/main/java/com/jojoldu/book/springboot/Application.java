970d056f36caf0a9ace4a93b05ec4870d8c3cf24
dokyu <dokyung36@naver.com>
2023-07-10 11:58:33 +0900

    Commit message EnableJpaAuditing을 추가 시 에러 발생 -> 각주 처리

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//@EnableJpaAuditing //jpa auditing 활성화
 @SpringBootApplication // 스프링 부트의 자동 설정, 스프링 bean읽기. 모두 자동 설정
 public class Application {
     public static void main(String[] args) {
